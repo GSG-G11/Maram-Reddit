05fa6cdc3913a6c60898f86225bd96cc64f740dd
Maram Ismail <maramismail2015@gmail.com>
2022-03-29 12:48:45 +0300

    create addpost query and addpost controller

--- server/controllers/PostsController.js
+++ server/controllers/PostsController.js
@@ -1,6 +1,12 @@
 const addPostQuery = require('../database/queries/PostsQueries');
 
 const addPost = (req, res) => {
-  addPostQuery(req.body).then((data) => console.log(data));
+  addPostQuery(req.body).then((data) => {
+    console.log(data);
+    res.status(201).json({
+      message: 'post added successfully',
+      post: data.rows[0],
+    });
+  }).catch(() => res.json({ message: 'error' }));
 };
 module.exports = addPost;
--- server/database/queries/PostsQueries.js
+++ server/database/queries/PostsQueries.js
@@ -1,9 +1,9 @@
 const connection = require('../config/connection');
 
-const addPostQuery = ({ username, title, content }) => {
+const addPostQuery = ({ user_id, title, content }) => {
   const sql = {
-    text: 'INSERT INTO posts (username,title,content) VALUES ($1, $2, $3) RETURNING *',
-    values: [username, title, content],
+    text: 'INSERT INTO posts (user_id, title,content)  VALUES ($1, $2, $3) Returning *',
+    values: [user_id, title, content],
   };
   return connection.query(sql);
 };
